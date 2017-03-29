b9e3bc8c6311dd9b56772d1fb6f459c9eeda364e
brials <brials008@hotmail.com>
2017-03-29 10:46:20 -0700

    added first route to new gallery-service file.

---
+++ app/service/gallery-service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = ['$q', '$log', '$http', 'authService', gallerySerivce];
+
+function gallerySerivce($q, $log, $http, authService){
+  $log.debug('gallerySerivce');
+
+  let service = {};
+  service.galleries = [];
+
+  service.createGallery = function(gallery){
+    $log.debug('gallerySerivce.createGallery');
+
+    return authService.getToken()
+    .then(token => {
+      let url = `${__API_URL__}/api/gallery`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      return $http.post(url, gallery, config);
+    })
+    .then(res => {
+      $log.log('gallery created');
+      let gallery = res.data;
+      service.galleries.unshift(gallery);
+      return gallery;
+    })
+    .catch(err => {
+      $log.error(err.message);
+    });
+  };
+}
