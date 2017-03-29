5cda8f0c189d272ce09fd5c41bf25f61aa1cacfc
Anna Ulanova <anetteua0@gmail.com>
2017-03-28 17:53:23 -0700

    working on buttons

--- lab-anna/webpack.config.js
+++ lab-anna/webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader'
       }
     ]
   }
