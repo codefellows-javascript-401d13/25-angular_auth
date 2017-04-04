25f0567352a7861d2cf808287e173fd2f2170c4e
caylazabel <caylazabel@gmail.com>
2017-04-03 22:27:34 -0700

    functionality working now, upload a pic works

--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -2,18 +2,25 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', '$rootScope', 'galleryService',  HomeController];
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController];
 
-function HomeController($log, $rootScope, galleryService){
+function HomeController($log, $rootScope, galleryService) {
   $log.debug('HomeController');
 
-  this.galleries=[];
+  this.galleries = [];
 
-  this.fetchGalleries = function(){
+  this.fetchGalleries = function() {
     galleryService.fetchGalleries()
     .then( galleries => {
       this.galleries = galleries;
+      this.currentGallery = galleries[0];
     });
+  };
+
+  this.galleryDeleteDone = function(gallery) {
+    if (this.currentGallery._id === gallery._id) {
+      this.currentGallery = null;
+    }
   };
 
   this.fetchGalleries();
