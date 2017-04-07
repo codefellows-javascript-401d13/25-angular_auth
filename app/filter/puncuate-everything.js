68bbbc24494d2c5ae7beb15fd1f73a1055b082e3
caylazabel <caylazabel@gmail.com>
2017-04-06 19:14:43 -0700

    pushing up basic things for the filter

--- app/filter/puncuate-everything.js
+++ app/filter/puncuate-everything.js
@@ -1,4 +1,18 @@
-// 'use strict';
-//
-// module.exports = function () {
-//   return function (galleries, punctuateTerm) {
+'use strict';
+
+
+module.exports = function() {
+  return function(galleries, searchTerm) {
+    let fuzzyRegex = generateFuzzyRegex(searchTerm);
+
+    return galleries.filter(gallery => {
+      return fuzzyRegex.test(gallery.name.toUpperCase());
+    });
+  };
+};
+
+function generateFuzzyRegex(input){
+  if(!input) return /.*/;
+  let fuzzyString = '.*' + input.toUpperCase().split('').join('.*') + '.*'; //split makes it an array, then join brings it back together into a string
+  return new RegExp(fuzzyString);
+}
